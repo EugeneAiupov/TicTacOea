d3229e6b20d4cc81ff08b445a6d054bc5ac4693c
Eugene Younge <jeka25736@gmail.com>
2023-07-16 20:02:10 +0300

    first commit

--- src/Game.java
+++ src/Game.java
@@ -2,5 +2,6 @@ public class Game {
     public static void main(String[] args) {
         Gui game = new Gui();
         System.out.println(game.hashCode());
+        // this is my first game
     }
 }
